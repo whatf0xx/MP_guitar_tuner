8e466fcd544eb6b6626398119b5fb810a69067ee
Fox <whatf0xx@gmail.com>
2022-03-21 12:16:04 +0000

    does this wokr we dont knw

---
+++ div_calcs.s
@@ -0,0 +1,70 @@
+#include <xc.inc>
+    
+    global	num1L, num1H, num2L, num2H, resL, resH, add_div, sub_div
+    
+    psect	udata_acs   ; named variables in access ram
+    num1L:	ds 1	; Lower byte of div_add
+    num1H:	ds 1	; Higher byte of div_add
+    num2L:	ds 1	; timer_count
+    num2H:	ds 1
+    resL:	ds 1	; Lower byte of addition result
+    resH:	ds 1	; Higher byte of addition result
+    
+    
+    
+    psect	div_calcs_code, class=CODE
+    
+    add_div:
+    
+	movff	num2L, resL	
+    
+	movf	num1L, W
+	addwf	resL, F		; num1L + num2
+	
+	movf	num1H, W	; store the higher byte in resH
+	addwfc	resH, F		; add the carry bit from previous calc to num1H and store in higher byte of result.
+	
+	
+	;or...
+	;movff	num1H, resH	;
+	;clrf	WREG		; clear the working registry from previous calc
+	;addwfc	resH		; so that only the carry bit is added to num1H and stored in higher byte of result
+	
+	return
+	
+    sub_div:
+	
+	movf	num2L, W	; move low byte of num2 to working reg
+	subwfb	num1L, W, A	; do num1L - num2L (with borrow) and store in resL
+	movwf	resL, A
+	
+	BTFSC	STATUS, 0x07	;is the carry bit 0?
+	bra	sub_step2	;no
+	return			; YES, return and result is stored in resL
+    
+    sub_step2:
+    
+	movlw	0x00
+	subfwb	resL, W, A
+	movwf	resL, A
+	return
+;add_div_test:
+	
+	;	;******Testing to see if add_div works with test values*******
+;	movlw	0x12	    ; setting up the variables
+;	movwf	num1H, A
+;	movlw	0xFF
+;	movwf	num1L, A
+;	movlw	0x46
+;	movwf	num2L, A
+;	call	add_div	    ; do the addition
+;	
+;	call	LCD_shift_cursor    ;preparing to write to LCD on next line
+;	movf	resH, W, A	    
+;	call	LCD_Write_Hex_orig  ;write higher byte to LCD
+;	movf	resL, W, A
+;	call	LCD_Write_Hex_orig  ; followed by lower byte
+;	; *******The add_div works!!!!! ********
+	
+
+end
--- main.s
+++ main.s
@@ -6,15 +6,19 @@ extrn	ADC_Setup, ADC_Read, Mult_16_16, Mult_8_24, convert_voltage, convert_volta
 extrn	ARG1L, ARG2L, ARG1H, ARG2H, RES0, RES1, RES2, RES3, ARG1M ;global variables from ADC
 extrn	timer_flag, time_counter, low_byte_thrsh, high_byte_thrsh
 extrn	timing_setup, is_low, is_high
+extrn	num1L, num1H, num2L, resL, resH, add_div, sub_div
 	
 psect	udata_acs   ; reserve data space in access ram
 	
 	raw_store	EQU 0x100
 	counter:	ds 1
-	div_co_low:	ds 1
-	div_co_mid:	ds 1
-	div_co_high:	ds 1
-	delta_t:	ds 1
+	div_add_low:	ds 1
+	div_add_high:	ds 1
+	div_co:		ds 1
+	ns_high:	ds 1
+	ns_low:		ds 1
+	delta:		ds 1
+	
     
 psect	code, abs
 	
@@ -27,12 +31,14 @@ setup:
 	call	ADC_Setup	; setup ADC
 
 	movlw	0
-	movwf	div_co_low, A	;start variable from 0 each run
-	movwf	div_co_mid, A
-	movwf	div_co_high, A
+	movwf	div_add_low, A	;start variable from 0 each run
+	movwf	div_add_high, A
+	movwf	div_co, A
 	
-	movlw	1
-	movwf	delta_t, A	;1/4th the delay
+	movlw	0x76		; setting up the sampling rate
+	movwf	ns_high, A
+	movlw	0x5F
+	movwf	ns_low, A
 	
 	goto	start
 	
@@ -41,9 +47,6 @@ start:
 	lfsr	0, raw_store	    ;store bits of raw data
     
 meas_loop:
-	
-	movf	delta_t, W, A	    ;take a measurement every delta_t x4us
-	call	LCD_delay_x4us
 	
 	call	ADC_Read
 	movff	ADRESL, POSTINC0, A	;low bit to RAM, increment FSR0
@@ -57,7 +60,6 @@ meas_loop:
 	call	is_low			;get to a good point to start timing
 	call	is_high			;start with the signal high
 	
-	
 	movlw	1
 	movwf	timer_flag, A		;start timing
 	
@@ -65,6 +67,55 @@ meas_loop:
 	call	is_high			;finish when the timer goes high again
 	
 Div_loop:
+  
+    
+	; Setting up the variables to be used by div_add
+	movff	div_add_high, num1H, A
+	movff	div_add_low, num1L, A
+	movff	time_counter, num2L, A
+	; add
+	call	add_div
+	movff	resH, div_add_high, A	;relabel so that they can
+	movff	resL, div_add_low, A	; be used by the loop again
+	
+	incf	div_co, A		;increment div_counter (m)
+
+	; need to do comparison - is div_add greater than ns?
+div_low:
+	movf	div_add_high, W, A	;high byte to W
+	cpfslt	ns_high, A		;is W > f?
+	bra	div_check_eq1		;NO, check equality
+	movff	div_add_low, num1L, A	;YES, find delta
+	movff	ns_low, num2L, A
+	call	sub_div
+	movff	resL, delta, A
+	goto	output
+	
+	
+	
+div_check_eq1:
+	cpfsgt	ns_high, A		;is W < f?
+	bra	div_check_low1		;it's equal - check the low byte
+	return				;it's lower - leave the loop
+	
+div_check_low1:
+	movf	div_add_low, W, A	;low byte to W
+	cpfslt	ns_low, A		;is W > f?
+	bra	Div_loop		;NO, add again
+	return				;YES, finish loop
+
+output:
+	call	LCD_clear
+	movlw	1
+	call	LCD_delay_ms
+	movf	div_co, W, A
+	call	LCD_Write_Hex_orig
+	
+	call	LCD_shift_cursor
+	movf	delta, W, A
+	call	LCD_Write_Hex_orig
+	
+	
 	
 	
 ;	call	LCD_clear
